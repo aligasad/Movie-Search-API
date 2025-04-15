8c723ee8edb8b9acfb0bb280014d9f5967747760
aligasad <asadalam4291@gmail.com>
2025-04-16 02:31:25 +0530

    This is a movie search api website

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,68 @@
-import React, { useEffect, useState } from 'react'
-import './App.css';
-import Card from './Card.jsx';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./Card.jsx";
+import axios from "axios";
 
-const data = [
-  {
-    poster: "https://image.tmdb.org/t/p/original/hu4psJoKxDYezvvS4GlLF531fom.jpg",
-    image: "https://image.tmdb.org/t/p/original/whkT53Sv2vKAUiknQ13pqcWaPXB.jpg",
-    name: "Bright",
-    release: "2017, David Ayer",
-    duration: "117 mins",
-    type: "Action Crime Fancy",
-    description: "Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work with an Orc to find a weapon everyone is prepared to kill for."
-  },
-  {
-    poster: "https://image.tmdb.org/t/p/original/ohijjhvNqAPKTURQr6VIF7xuAjY.jpg",
-    image: "https://image.tmdb.org/t/p/original/s4Qn5LF6OwK4rIifmthIDtbqDSs.jpg",
-    name: "Tomb Raider",
-    release: "2018, Roar Uthaug",
-    duration: "117 mins",
-    type: "Action Crime Fancy",
-    description: "Lara Croft, the fiercely independent daughter of a missing adventurer, must push herself beyond her limits when she finds herself on the island where her father disappeared."
-  },
-];
+// const data = [
+//   {
+//     poster: "https://image.tmdb.org/t/p/original/hu4psJoKxDYezvvS4GlLF531fom.jpg",
+//     image: "https://image.tmdb.org/t/p/original/whkT53Sv2vKAUiknQ13pqcWaPXB.jpg",
+//     name: "Bright",
+//     release: "2017, David Ayer",
+//     duration: "117 mins",
+//     type: "Action Crime Fancy",
+//     description: "Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work with an Orc to find a weapon everyone is prepared to kill for."
+//   },
+//   {
+//     poster: "https://image.tmdb.org/t/p/original/ohijjhvNqAPKTURQr6VIF7xuAjY.jpg",
+//     image: "https://image.tmdb.org/t/p/original/s4Qn5LF6OwK4rIifmthIDtbqDSs.jpg",
+//     name: "Tomb Raider",
+//     release: "2018, Roar Uthaug",
+//     duration: "117 mins",
+//     type: "Action Crime Fancy",
+//     description: "Lara Croft, the fiercely independent daughter of a missing adventurer, must push herself beyond her limits when she finds herself on the island where her father disappeared."
+//   },
+// ];
 
 function App() {
-  // HERE I WAS FETCHING DATA FROM MOVIE API
-  // const API_KEY = "b68cd8803f76c38479a47b7a2077e5e8"; //for adding api we add this: &api_key=
-  // const toCompleteURL = "https://image.tmdb.org/t/p/original"; 
-  // const fetchApiData = async (url) => {
-  //   try{
-  //     const response = await fetch(url);
-  //     const result = await response.json();
-  //     console.log(result.results);
-  //   } 
-  //   catch(error) {
-  //     console.log(error);
-  //   }
-  // }
-  
-  // useEffect(()=>{
-  //   fetchApiData(("https://api.themoviedb.org/3/search/movie?query=" + `${"tomb raider"}` + "&include_adult=false&language=en-US&page=1&api_key="+ API_KEY));
-  // })
+  const [data, setData] = useState([]);
+  const [name, setName] = useState("");
 
-  const [arr, setArr] = useState(data);
+  // HERE I WAS FETCHING DATA FROM MOVIE API
+  const API_KEY = "b68cd8803f76c38479a47b7a2077e5e8"; //for adding api we add this: &api_key=
+  const toCompleteURL = "https://image.tmdb.org/t/p/original";
+
+  async function handleData() {
+    const result = await axios.get(
+      "https://api.themoviedb.org/3/search/movie?query=" +
+        `${name}` +
+        "&include_adult=false&language=en-US&page=1&api_key=" +
+        API_KEY
+    );
+    setData(result.data.results);
+
+    setName("");
+  }
 
   return (
     <>
+      <div className="inputBox">
+        <h1>Search Your Favorite Movie </h1>
+        <div className="inpt">
+          <input
+            type="text"
+            placeholder="Enter a movie name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button onClick={handleData}>Search</button>
+        </div>
+      </div>
       <div className="main">
-        <Card arr={arr} />
+        <Card arr={data} url={toCompleteURL} />
       </div>
     </>
-  )
+  );
 }
 
 export default App;
--- src/Card.jsx
+++ src/Card.jsx
@@ -4,35 +4,55 @@ import { FaMessage } from "react-icons/fa6";
 
 function Card(props) {
   const data = props.arr;
+  const imgUrl = props.url;
+  // function trimTitle(title, len) {
+  //   return title.lenght > len ? title.split(" ").splice(0, len + 1).join(" ") : title
+  // }
+
+  function trimContent(title, length) {
+    return title.length > length
+      ? title
+          .split(" ")
+          .slice(0, length - 1)
+          .join(" ") + "..."
+      : title;
+  }
+
   return (
     <>
       {data.map((obj) => {
         return (
-          <div className="card">
+          <div key={obj.id} className="card">
             <div className="left">
               <p className="icons">
-                <span><IoShareSocialSharp /></span>
-                <span><FaHeart /></span>
-                <span><FaMessage /></span>
+                <span>
+                  <IoShareSocialSharp />
+                </span>
+                <span>
+                  <FaHeart />
+                </span>
+                <span>
+                  <FaMessage />
+                </span>
               </p>
               <div className="upper">
-                <img src={obj.image} alt="" />
+                <img src={imgUrl + "" + obj.poster_path} alt="" />
                 <div className="data">
-                  <h1>{obj.name}</h1>
-                  <p>{obj.release}</p>
+                  <h1>{trimContent(obj.original_title, 4)}</h1>
+                  <p>{obj.release_date}</p>
 
-                  <p className="time"><button>{obj.duration}</button> {obj.type}</p>
+                  <p className="time">
+                    <button>{obj.popularity}</button> {trimContent(obj.title, 4)}
+                  </p>
                 </div>
               </div>
               <div className="lower">
-                <p>{obj.description}</p>
+                <p>{trimContent(obj.overview, 20)}</p>
               </div>
             </div>
             <div className="right">
-              <img src={obj.poster} alt="" />
+              <img src={imgUrl + "" + obj.backdrop_path} alt="image not found" />
             </div>
-
-            
           </div>
         );
       })}
